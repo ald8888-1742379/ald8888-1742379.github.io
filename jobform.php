b9bf98f7535aa6306e4b06ce875b70ca838f4fb6
ANDREA DANG <ald8888@cs.washington.edu>
2020-04-30 10:50:09 -0700

    created job form

---
+++ jobform.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+
+        Applicant <?php echo $_GET["name"]; ?><br>
+        Email address: <?php echo $_GET["email"]; ?><br>
+        Street Address: <?php echo $_GET["street"]; ?><br>
+
+    </body>
+</html>
